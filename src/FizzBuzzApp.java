909499e4d648f3d1855ad5e1ca21fc8b96ef6970
Maureen Ballard <moballard@comcast.net>
2024-08-31 19:00:21 -0600

    Working FizzBuxx project

--- src/FizzBuzzApp.java
+++ src/FizzBuzzApp.java
@@ -1,4 +1,26 @@
 
 public class FizzBuzzApp {
+	
+	public static void main(String[] args) {
+	
+//	int index = 1;
+	for (int index=1; index < 100; index++)
+	{
+		   int threemod = index % 3;
+		   int fivemod = index % 5;
+		   
+	   if (threemod == 0 && fivemod == 0){
+         System.out.println("FizzBuzz");  
+	   }
+	   else if (threemod == 0) {
+		   System.out.println("Fizz");
+	   }
+	   else if (fivemod == 0) {
+		   System.out.println("Buzz");
+	   }
+	   else System.out.println(index);
+	 } 
 
+  }
+	
 }
